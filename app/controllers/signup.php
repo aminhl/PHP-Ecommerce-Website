83fcf48bb11f1260e506bc8ee413b02ce4c41c9e
aminhl <amin.hlel@esprit.tn>
2021-08-29 11:30:57 +0100

    Database And Users Table

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -5,6 +5,7 @@ class Signup extends Controller{
         $data['page_title'] = 'Signup';
         if ($_SERVER['REQUEST_METHOD'] == 'POST'){
             show($_POST);
+            $User = $this->load_model('User');
         }
         $this->View('signup',$data);
     }
--- app/core/controller.php
+++ app/core/controller.php
@@ -6,4 +6,11 @@ class Controller{
             include_once '../app/views/' . THEME .$path . '.php';
         }
     }
+    public function load_model($model){
+        if (file_exists('../app/models/' . strtolower($model) . '.class.php')){
+            include_once '../app/models/' . strtolower($model) . '.class.php';
+            return $a = new $model();
+        }
+       return false;
+    }
 }
