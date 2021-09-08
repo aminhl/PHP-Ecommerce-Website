ba35e6bd1d508b9443b4fe884286adf77ad51e1c
aminhl <amin.hlel@esprit.tn>
2021-09-08 19:43:37 +0100

    Adding The Admin Template

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -1,13 +1,15 @@
 <?php
 
-class Home extends Controller {
-    public function index($a = '',$b = ''){
+class Admin extends Controller
+{
+    public function index($a = '', $b = '')
+    {
         $user = $this->load_model('user');
         $user_data = $user->check_login();
         if (is_object($user_data)){
             $data['user_data'] = $user_data ;
         }
-        $data['page_title'] = 'Home';
-        $this->view('index',$data);
+        $data['page_title'] = 'Admin';
+        $this->view('admin/index', $data);
     }
 }
